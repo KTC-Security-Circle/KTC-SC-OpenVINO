bac9b30751975c7f1effaa7f05704ff38fb9e535
Shota <104074879+Shota-Kizaki@users.noreply.github.com>
2024-01-24 02:35:05 +0000

    ok

--- test/myproject/videoupload/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/', views.upload_video, name='upload_video'),
-    path('videos/', views.video_list, name='video_list'),
-    # 他のURLパターンがあればここに追加
-]
