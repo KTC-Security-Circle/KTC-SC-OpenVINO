a6dae97c54bf71c941b6915aa55274c970860f5d
Shota-Kizaki <104074879+Shota-Kizaki@users.noreply.github.com>
2024-02-07 18:06:23 +0900

    jj

--- KTC-SC-OpenVINO/app/views.py
+++ KTC-SC-OpenVINO/app/views.py
@@ -3,7 +3,7 @@ from django.views.generic import View
 from .forms import FileUploadSampleForm
 from django.http import JsonResponse
 from django.urls import reverse
-from .application import object_detection_django 
+from .application import object_detection_django ,segmentation_django
 import os
 def index(request):
     return render(request, 'index.html')
@@ -34,20 +34,45 @@ class IndexView(View):
         redirect_url = reverse('obj_result')
         # 処理が完了したら、リダイレクト先のURLを含むJSONを返す
         return JsonResponse({'redirectUrl': redirect_url})
+    
+class seg_demo(View):
+    def get(self, request, *args, **kwargs):
+        form = FileUploadSampleForm()
+        return render(request, 'segmentation/demo.html', {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        """POSTリクエスト用のメソッド"""
+        form = FileUploadSampleForm(request.POST, request.FILES)
+
+        if not form.is_valid():
+            return render(request, 'segmentation/demo.html', {"form": form})
+
+        filename_save = form.save()
+        
+        context = {
+            'form': form,
+            'filename_save': filename_save,
+        }
+        
+        output_video_path = os.path.join('media', 'upload', 'output.mp4')
+        input_video_path = os.path.join('media', 'upload', 'input.mp4')
+        segmentation_django.run_segmentation(input_video_path,output_video_path)
+        #zzz = ai.run()
+        redirect_url = reverse('seg_result')
+        # 処理が完了したら、リダイレクト先のURLを含むJSONを返す
+        return JsonResponse({'redirectUrl': redirect_url})
 
 
 
 # IndexViewクラスのインスタンスを作成し、関数ビューとして使用可能にする
 obj_demo = IndexView.as_view()
 
+
 # 物体検出結果ページのビュー関数
 def obj_result(request):
     return render(request, 'object_detection/result.html')
 
-# セグメンテーションデモページのビュー関数
-def seg_demo(request):
-    return render(request, 'segmentation/demo.html')
-
 # セグメンテーション結果ページのビュー関数
 def seg_result(request):
     return render(request, 'segmentation/result.html')
+
