1fa7131e3c89f065a3018ff4860b941cb6d5c511
fhazuki <ktc23a31e0016@edu.kyoto-tech.ac.jp>
2024-02-07 15:22:47 +0900

    segmentation

--- KTC-SC-OpenVINO/.venv/Scripts/activate.bat
+++ KTC-SC-OpenVINO/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\krake\OneDrive\ドキュメント\GitHub\KTC-SC-OpenVINO\KTC-SC-OpenVINO\.venv
+set VIRTUAL_ENV=C:\Users\ktc\Documents\GitHub\KTC-SC-OpenVINO\KTC-SC-OpenVINO\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
